7a201e0fb885e181c83806a66760dc9d94c7091a
John Villamar <xjohnishere19@aol.com>
2016-09-08 21:47:47 -0400

    Add condition if session is present. Therefore, users won't see the login screen when loading the site.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,10 @@ public class Application extends Controller {
 
     public Result index()
     {
-        return ok(index.render("Your new application is ready."));
+        if (!session().containsKey("user_id") )
+            return ok(index.render("Your new application is ready."));
+        else
+            return redirect(routes.userProfile.index(Long.parseLong(session().get("user_id")), session().get("user_id") ) );
     }
 
 
